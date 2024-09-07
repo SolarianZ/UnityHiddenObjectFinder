bcd90ca1c83c53225c6a9d72959b1a23b7835e7d
ZQY <VDerGoW@hotmail.com>
2024-09-07 14:31:03 +0800

    feat: Hierarchy path builder

--- Editor/Scripts/HiddenObjectFinder.cs
+++ Editor/Scripts/HiddenObjectFinder.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using UnityEngine;
-using UnityEngine.Assertions;
 using UnityEngine.SceneManagement;
 using UObject = UnityEngine.Object;
 
@@ -91,74 +89,6 @@ namespace GBG.HiddenObjectFinder.Editor
         public static bool MatchHideFlags(this UObject obj, HideFlags hideFlagsFilter)
         {
             return obj && (obj.hideFlags & hideFlagsFilter) != 0;
-        }
-
-
-        public static string BuildHierarchyPath(this Component component)
-        {
-            if (!component)
-            {
-                throw new ArgumentNullException(nameof(component));
-            }
-
-            Transform transform = component.transform;
-            StringBuilder builder = new StringBuilder();
-            builder.Append(transform.name);
-
-            Transform parent = transform.parent;
-            while (parent)
-            {
-                builder.Insert(0, '/').Insert(0, parent.name);
-                parent = parent.parent;
-            }
-
-            return builder.ToString();
-        }
-
-        public static string BuildHierarchyPath(this GameObject go)
-        {
-            if (!go)
-            {
-                throw new ArgumentNullException(nameof(go));
-            }
-
-            return go.transform.BuildHierarchyPath();
-        }
-
-        public static List<string> BuildHierarchyPathParts(this Component component)
-        {
-            if (!component)
-            {
-                throw new ArgumentNullException(nameof(component));
-            }
-
-            List<string> pathParts = new List<string>();
-            component.BuildHierarchyPathInternal(pathParts);
-
-            return pathParts;
-        }
-
-        public static List<string> BuildHierarchyPathParts(this GameObject go)
-        {
-            if (!go)
-            {
-                throw new ArgumentNullException(nameof(go));
-            }
-
-            return go.transform.BuildHierarchyPathParts();
-        }
-
-        private static void BuildHierarchyPathInternal(this Component component, List<string> pathParts)
-        {
-            Assert.IsNotNull(component);
-            Assert.IsNotNull(pathParts);
-
-            Transform transform = component.transform;
-            while (transform)
-            {
-                pathParts.Insert(0, transform.name);
-                transform = transform.parent;
-            }
         }
     }
 }
---
+++ Editor/Scripts/HierarchyPathBuilder.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace GBG.HiddenObjectFinder.Editor
+{
+    public static class HierarchyPathBuilder
+    {
+        public static string BuildHierarchyPath(this Component component)
+        {
+            if (!component)
+            {
+                throw new ArgumentNullException(nameof(component));
+            }
+
+            Transform transform = component.transform;
+            StringBuilder builder = new StringBuilder();
+            builder.Append(transform.name);
+
+            Transform parent = transform.parent;
+            while (parent)
+            {
+                builder.Insert(0, '/').Insert(0, parent.name);
+                parent = parent.parent;
+            }
+
+            return builder.ToString();
+        }
+
+        public static string BuildHierarchyPath(this GameObject go)
+        {
+            if (!go)
+            {
+                throw new ArgumentNullException(nameof(go));
+            }
+
+            return go.transform.BuildHierarchyPath();
+        }
+
+        public static List<string> BuildHierarchyPathParts(this Component component)
+        {
+            if (!component)
+            {
+                throw new ArgumentNullException(nameof(component));
+            }
+
+            List<string> pathParts = new List<string>();
+            component.BuildHierarchyPathInternal(pathParts);
+
+            return pathParts;
+        }
+
+        public static List<string> BuildHierarchyPathParts(this GameObject go)
+        {
+            if (!go)
+            {
+                throw new ArgumentNullException(nameof(go));
+            }
+
+            return go.transform.BuildHierarchyPathParts();
+        }
+
+        private static void BuildHierarchyPathInternal(this Component component, List<string> pathParts)
+        {
+            Assert.IsNotNull(component);
+            Assert.IsNotNull(pathParts);
+
+            Transform transform = component.transform;
+            while (transform)
+            {
+                pathParts.Insert(0, transform.name);
+                transform = transform.parent;
+            }
+        }
+    }
+}
