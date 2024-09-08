41eb40ed379cfe4211c9bb06d4a942e25a50b1b2
ZQY <VDerGoW@hotmail.com>
2024-09-08 11:24:22 +0800

    feat: Hidden object operations

--- Editor/Scripts/HiddenObjectInspectorElement.cs
+++ Editor/Scripts/HiddenObjectInspectorElement.cs
@@ -1,4 +1,6 @@
-﻿using UnityEditor.UIElements;
+﻿using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
 using UnityEngine.UIElements;
 using UObject = UnityEngine.Object;
 
@@ -7,21 +9,46 @@ namespace GBG.HiddenObjectFinder.Editor
     public class HiddenObjectInspectorElement : VisualElement
     {
         private InspectorElement _inspector;
+        private UObject _object;
+        private HideFlags _newHideFlags;
 
 
         public HiddenObjectInspectorElement()
         {
-            
+            IMGUIContainer operationDrawer = new IMGUIContainer(DrawObjectOperations);
+            Add(operationDrawer);
+        }
+
+        private void DrawObjectOperations()
+        {
+            EditorGUILayout.EnumFlagsField("Current Hide Flags", _object ? _object.hideFlags : HideFlags.None);
+
+            bool targetObjModifiable = _object && !EditorUtility.IsPersistent(_object);
+            EditorGUI.BeginDisabledGroup(!targetObjModifiable);
+            {
+                _newHideFlags = (HideFlags)EditorGUILayout.EnumFlagsField("New Hide Flags", _newHideFlags);
+
+                EditorGUILayout.Space();
+                if (GUILayout.Button("Set New Hide Flags"))
+                {
+                    Undo.RecordObject(_object, "Set HideFlags");
+                    _object.hideFlags = _newHideFlags;
+
+                    EditorApplication.RepaintHierarchyWindow();
+                }
+            }
+            EditorGUI.EndDisabledGroup();
         }
 
         public void SetTarget(UObject obj)
         {
+            _object = obj;
             if (_inspector != null)
             {
                 Remove(_inspector);
             }
 
-            _inspector = new InspectorElement(obj)
+            _inspector = new InspectorElement(_object)
             {
                 name = "ObjectInspector",
                 style =
